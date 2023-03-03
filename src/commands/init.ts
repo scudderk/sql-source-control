6399c1071b3268f59a6192603bf56612df0d94ac
Kyle Scudder <kyle.s@paloma.co.uk>
2023-03-03 17:10:40 +0000

    Rewrote init command to take into config changes

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,8 +1,8 @@
 import * as inquirer from 'inquirer';
+import { Connection } from 'tedious';
 
 import Config from '../common/config';
-//import Connection from '../common/connection';
-import { IConnection } from '../common/interfaces';
+import Setting from '../common/setting';
 import { PathChoices } from './eums';
 import { InitOptions } from './interfaces';
 
@@ -13,29 +13,29 @@ export default class Init {
    * Invoke action.
    */
   invoke() {
-    /*const webConfigConns = Config.getConnectionsFromWebConfig(
+    const webConfigConns = Config.getConnectionsFromWebConfig(
       this.options.webconfig
     );
-    const conn = new Connection();
+    const sett = new Setting();
 
     if (!this.options.force && Config.doesDefaultExist()) {
       // don't overwrite existing config file
       return console.error('Config file already exists!');
     }
 
-    if (webConfigConns) {
-      // use options from web config
-      conn.loadFromObject(webConfigConns[0]);
-    }
+    //if (webConfigConns) {
+    //  // use options from web config
+    //  sett.loadFromObject(webConfigConns[0]);
+    //}
 
-    if (this.options.skip) {
-      // skip prompts and create with defaults
-      Config.write({ connections: this.options.webconfig || [conn] });
-      return;
-    }
+    //if (this.options.skip) {
+    //  // skip prompts and create with defaults
+    //  Config.write({ connections: this.options.webconfig || [conn] });
+    //  return;
+    //}
     const prompt = inquirer.createPromptModule();
-    return prompt(this.getQuestions(conn, !!webConfigConns))
-      .then((answers) => this.writeFiles(answers));*/
+    return prompt(this.getQuestions(sett, !!webConfigConns))
+      .then((answers) => this.writeFiles(answers));
     return console.error('Code needs to be amended!');
   }
 
@@ -44,7 +44,7 @@ export default class Init {
    *
    * @param conn Connection object to use for default values.
    */
-  /*private getQuestions(conn: Connection, showWebConfig: boolean) {
+  private getQuestions(sett: Setting, showWebConfig: boolean) {
     const questions: inquirer.QuestionCollection = [
       {
         choices: () => this.getPathChoices(showWebConfig),
@@ -53,35 +53,95 @@ export default class Init {
         type: 'list',
       },
       {
-        default: conn.server || undefined,
+        default: 'localhost',
         message: 'Server URL.',
         name: 'server',
         when: (answers) => answers.path !== PathChoices.WebConfig,
       },
       {
-        default: conn.port || undefined,
+        default: '1433',
         message: 'Server port.',
         name: 'port',
         type: 'number',
         when: (answers) => answers.path !== PathChoices.WebConfig,
       },
       {
-        default: conn.database || undefined,
+        default: 'database',
         message: 'Database name.',
         name: 'database',
         when: (answers) => answers.path !== PathChoices.WebConfig,
       },
       {
-        default: conn.user || undefined,
+        default: 'username',
         message: 'Login username.',
         name: 'user',
         when: (answers) => answers.path !== PathChoices.WebConfig,
       },
       {
-        default: conn.password || undefined,
+        default: 'super_secure_password',
         message: 'Login password.',
         name: 'password',
         type: 'password',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: '0.0.1',
+        message: 'Current version.',
+        name: 'currentVersion',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'procs',
+        message: 'Procedure folder.',
+        name: 'procs',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'data',
+        message: 'Data folder.',
+        name: 'data',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'functions',
+        message: 'Functions folder.',
+        name: 'functions',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'jobs',
+        message: 'Jobs folder.',
+        name: 'jobs',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'schema',
+        message: 'schemas folder.',
+        name: 'schemas',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'tables',
+        message: 'Tables folder.',
+        name: 'tables',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'triggers',
+        message: 'Trigger folder.',
+        name: 'triggers',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'types',
+        message: 'Types folder.',
+        name: 'types',
+        when: (answers) => answers.path !== PathChoices.WebConfig,
+      },
+      {
+        default: 'views',
+        message: 'Views folder.',
+        name: 'views',
         when: (answers) => answers.path !== PathChoices.WebConfig,
       },
       {
@@ -93,7 +153,7 @@ export default class Init {
     ];
 
     return questions;
-  }*/
+  }
 
   /**
    * Get all available configuration file path choices.
@@ -128,21 +188,44 @@ export default class Init {
    * @param answers Answers from questions.
    */
   private writeFiles(answers: inquirer.Answers) {
-    const conn: IConnection = {
-      database: answers.database,
-      password: answers.password,
-      port: answers.port,
-      server: answers.server,
-      user: answers.user
+    const sett: Setting = {
+      name: answers.name,
+      connection: {
+        server: answers.server,
+        database: answers.database,
+        port: answers.port,
+        user: answers.user,
+        password: answers.password,
+      },
+      output: {
+        root: answers.path,
+        procs: answers.procs,
+        data: answers.data,
+        functions: answers.functions,
+        jobs: answers.jobs,
+        schemas: answers.schemas,
+        tables: answers.tables,
+        triggers: answers.triggers,
+        types: answers.types,
+        views: answers.views,
+        temps: 'temp_files',
+      },
+      currentVersion: answers.currentVersion,
+      idempotency: {
+        procs: "if-exists-drop"
+      },
+      includeConstraintName: true,
+      loadFromObject: null,
+      loadFromString: null
     };
 
     if (answers.path === PathChoices.WebConfig) {
       Config.write({ connections: Config.defaultWebConfigFile });
     } else if (answers.path === PathChoices.ConnsConfig) {
       Config.write({ connections: Config.defaultConnectionsJsonFile });
-      Config.write({ connections: [conn] }, Config.defaultConnectionsJsonFile);
+      Config.write({ settings: [sett] }, Config.defaultConnectionsJsonFile);
     } else {
-      Config.write({ connections: [conn] });
+      Config.write({ settings: [sett] });
     }
   }
 }
