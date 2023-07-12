f6367e09ab828e69f434d713e701cbabdfa52a19
Kyle Scudder <kyle.s@paloma.co.uk>
2023-07-12 10:44:48 +0100

    Write sp changes to version folder

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,19 +1,26 @@
+import sql from 'mssql';
+import multimatch from 'multimatch';
 import path from 'path';
 import Config from '../common/config';
 import Setting from '../common/setting';
 import fs from 'fs';
 import chokidar from 'chokidar';
+import { StartOptions } from './interfaces';
+import { SqlObject, SqlPermissions, SqlTable } from '../queries/interfaces';
+import { objectRead, permissionsRead } from '../queries/mssql';
+import MSSQLGenerator from '../generators/mssql';
+import FileUtility from '../common/file-utility';
 
 export default class Start {
-  constructor() { }
+  constructor(private options: StartOptions) {}
 
   /**
    * Invoke action.
    */
   invoke() {
-    const config = new Config();
+    const config = new Config(this.options.config);
     const setts = config.getSettings();
-    console.log(setts);
+
     setts.forEach((sett: Setting) => {
       let temps = '';
       if (!fs.existsSync(sett.output.root)) {
@@ -40,16 +47,97 @@ export default class Start {
             storedProcedureName,
             'has been triggered'
           );
+
+          // connect to db
+          return new sql.ConnectionPool(sett.connection)
+            .connect()
+            .then((pool) => {
+              console.log(pool)
+              const queries: any[] = [
+                pool.request().query(objectRead(type, storedProcedureName)),
+                pool.request().query(permissionsRead),
+              ];
+              return Promise.all<sql.IResult<any>>(queries)
+                .then((results) => {
+                  const tables: sql.IRecordSet<SqlTable> = results[1].recordset;
+                  return results;
+                })
+                .then((results) => {
+                  pool.close();
+                  fs.unlink(path, (err) => {
+                    if (err) {
+                      console.error(err);
+                    }
+                  });
+                  return results;
+                });
+            })
+            .then((results) =>
+              writeFiles(config, sett, results, storedProcedureName, type)
+            )
+            .catch((error) => {
+              console.error(error);
+            });
         })
         .on('change', function (path) {
           console.log('File', path, 'has been changed');
-        })
-        .on('unlink', function (path) {
-          console.log('File', path, 'has been removed');
         })
         .on('error', function (error) {
           console.error('Error happened', error);
         });
     });
   }
+}
+function writeFiles(
+  config: Config,
+  sett: Setting,
+  results: any[],
+  name: string,
+  type: string
+) {
+  // note: array order MUST match query promise array
+  const objects: SqlObject[] = results[0].recordset;
+  const permissions: SqlPermissions[] = results[1].recordset
+    ? results[1].recordset
+    : [];
+
+  const generator = new MSSQLGenerator(config);
+  const file = new FileUtility(config, sett);
+  let content: string;
+  switch (type) {
+    // stored procedures
+    case 'P':
+      name = `${name}.sql`;
+      content = generator.storedProcedure(objects[0], sett);
+      content += generator.permissions(permissions, name.split('.')[0]);
+      file.write(`${sett.currentVersion}/${sett.output.procs}`, name, content);
+      file.write(`${sett.output.procs}`, name, content);
+      break;
+    // views
+    case 'V':
+      name = `${name}.sql`;
+      content = generator.view(objects[0], sett);
+      file.write(`${sett.currentVersion}/${sett.output.views}`, name, content);
+      file.write(sett.output.views, name, content);
+      break;
+    // functions
+    case 'TF':
+    case 'IF':
+    case 'FN':
+      name = `${name}.sql`;
+      content = generator.function(objects[0], sett);
+      file.write(
+        `${sett.currentVersion}/${sett.output.functions}`,
+        name,
+        content
+      );
+      file.write(sett.output.functions, name, content);
+      break;
+    default:
+      break;
+  }
+  
+  file.writeUpdate(generator, file);  
+  const msg = file.finalize();
+  console.log(msg);
 }
